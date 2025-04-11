be977ee33d0bfb672d412952e9e7a5f0c7481a69
Jason <jasongtse@gmail.com>
2025-04-11 19:40:31 -0230

    Added and modified controller and repository files for AutoQuote to work similar to HomeQuote. All POST operations regarding Users, Customers, Admins, Agents, AutoQuotes and HomeQuotes are now working. QuoteController.java has been commented out as all REST operations regarding quotes are now handled in AutoQuoteController.java and HomeQuoteController.java

--- src/main/java/com/example/capstoneproject/Controllers/QuoteController.java
+++ src/main/java/com/example/capstoneproject/Controllers/QuoteController.java
@@ -1,47 +1,47 @@
-package com.example.capstoneproject.Controllers;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import com.example.capstoneproject.DataAccess.QuoteRepository;
-import com.example.capstoneproject.Quotes.Quote;
-
-import java.util.List;
-
-import static com.example.capstoneproject.Controllers.RESTNouns.*;
-
-@RestController
-@RequestMapping(VERSION_1 + QUOTES)
-public class QuoteController {
-    @Autowired
-    private QuoteRepository quoteRepository;
-
-    @GetMapping
-    public List<Quote> getAllQuotes() {
-        return quoteRepository.findAll();
-    }
-
-    @GetMapping(ID)
-    public Quote getQuoteById(@PathVariable int id) {
-        return quoteRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Quote not found with ID: " + id));
-    }
-
-    @PostMapping
-    public Quote createQuote(@RequestBody Quote quote) {
-        return quoteRepository.save(quote);
-    }
-
-    @PutMapping(ID)
-    public Quote updateQuote(@PathVariable int id, @RequestBody Quote updatedQuote) {
-        Quote existingQuote = quoteRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Quote not found with ID: " + id));
-        existingQuote.setQuoteType(updatedQuote.getQuoteType());
-        existingQuote.setQuotePrice(updatedQuote.getQuotePrice());
-        existingQuote.setExpiryDate(updatedQuote.getExpiryDate());
-        existingQuote.setPaid(updatedQuote.isPaid());
-        return quoteRepository.save(existingQuote);
-    }
-
-    @DeleteMapping(ID)
-    public void deleteQuote(@PathVariable int id) {
-        quoteRepository.deleteById(id);
-    }
-}
+//package com.example.capstoneproject.Controllers;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//import com.example.capstoneproject.DataAccess.QuoteRepository;
+//import com.example.capstoneproject.Quotes.Quote;
+//
+//import java.util.List;
+//
+//import static com.example.capstoneproject.Controllers.RESTNouns.*;
+//
+//@RestController
+//@RequestMapping(VERSION_1 + QUOTES)
+//public class QuoteController {
+//    @Autowired
+//    private QuoteRepository quoteRepository;
+//
+//    @GetMapping
+//    public List<Quote> getAllQuotes() {
+//        return quoteRepository.findAll();
+//    }
+//
+//    @GetMapping(ID)
+//    public Quote getQuoteById(@PathVariable int id) {
+//        return quoteRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Quote not found with ID: " + id));
+//    }
+//
+//    @PostMapping
+//    public Quote createQuote(@RequestBody Quote quote) {
+//        return quoteRepository.save(quote);
+//    }
+//
+//    @PutMapping(ID)
+//    public Quote updateQuote(@PathVariable int id, @RequestBody Quote updatedQuote) {
+//        Quote existingQuote = quoteRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Quote not found with ID: " + id));
+//        existingQuote.setQuoteType(updatedQuote.getQuoteType());
+//        existingQuote.setQuotePrice(updatedQuote.getQuotePrice());
+//        existingQuote.setExpiryDate(updatedQuote.getExpiryDate());
+//        existingQuote.setPaid(updatedQuote.isPaid());
+//        return quoteRepository.save(existingQuote);
+//    }
+//
+//    @DeleteMapping(ID)
+//    public void deleteQuote(@PathVariable int id) {
+//        quoteRepository.deleteById(id);
+//    }
+//}
